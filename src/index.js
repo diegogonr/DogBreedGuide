363bb6003761d1ab397fdd7d95dc16491b3cc2e1
DgonzalesR30 <gonzalesrodriguezdiego@gmail.com>
2023-11-21 14:44:41 -0500

    update

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { Provider } from 'react-redux';
 import {BrowserRouter} from 'react-router-dom';
 import axios from 'axios';
 
-axios.defaults.baseURL = 'https://dog-web.onrender.com/'
+axios.defaults.baseURL = 'https://dog-web.onrender.com'
 
 ReactDOM.render(
   <React.StrictMode>
